dc87e253a8c61675c8832034c41e0e573e160a42
smhasnanmonir <monirhasnan@gmail.com>
2024-12-13 15:50:37 +0600

    Image changed on home

--- src/app/components/Home/HeroSection/HeroSection.jsx
+++ src/app/components/Home/HeroSection/HeroSection.jsx
@@ -3,15 +3,19 @@ import heroIcon from "../../../../photos/hero.png";
 
 const HeroSection = () => {
   return (
-    <div className="md:px-[2%] px-[3%] pt-[16px]">
+    <div className="md:px-[4%] px-[4%] pt-[16px]">
       <div className="grid md:grid-cols-3 grid-cols-1 place-items-center pt-[10px] md:gap-x-4 md:gap-y-0 gap-y-2">
         <div className="space-y-[32px] md:px-[40px]  col-span-2">
           <h1 className="md:text-5xl text-2xl font-semibold ">
             Welcome To Binary Shop!
           </h1>
-          <p className="text-3xl rounded-md leading-[46px]">
+          <p className="md:text-3xl text-2xl rounded-md md:leading-[46px] leading-[30px]">
             Binary Shop is a leading platform where you can find shared account
-            at <span className="font-extrabold ">Best Price</span>.
+            at the{" "}
+            <span className="font-extrabold underline underline-offset-[6px] decoration-pink-500 text-green-500">
+              Best Price
+            </span>
+            .
           </p>
 
           <p className="text-2xl underline decoration-pink-500 underline-offset-[8px] md:block hidden">
--- src/app/components/Home/TopProduct/TopProduct.jsx
+++ src/app/components/Home/TopProduct/TopProduct.jsx
@@ -2,70 +2,54 @@
 
 const TopProduct = () => {
   return (
-    <div className="py-[20px]">
-      <h1 className="text-center font-semibold text-3xl md:pb-[34px] pb-[16px] gap-3">
+    <div className="">
+      <h1 className="text-center font-semibold md:text-3xl text-2xl gap-3 py-[30px]">
         Our Best Products At a glance
       </h1>
 
-      <div className="grid md:grid-cols-4 grid-cols-3 place-items-center px-[5%]">
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
+      <div className="grid md:grid-cols-3 grid-cols-2 place-items-center px-[5%] gap-[2%]">
+        <div className="object-cover border rounded-lg p-5 hover:bg-red-200 transition-all duration-500">
           <img
-            className="w-[225px] object-cover "
-            src="https://utfs.io/f/ZSu459z2V7Otx2G3P4gRd5iYMXcK4E6C2DrvAo13fhL7lusW"
+            className="rounded-lg"
+            src="https://utfs.io/f/ZSu459z2V7OtBAiJDeagwv31OhZCzKN2qUWSLoM4p5lkjeG6"
             alt="Pure Vpn"
           />
         </div>
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
+        <div className="object-cover border rounded-lg p-5 hover:bg-red-200 transition-all duration-500">
           <img
-            className="w-[125px] "
-            src="https://utfs.io/f/ZSu459z2V7OtVvInm5Jl8P6Tt92ws7xReUp5WDqIYAfJkh0M"
+            className=""
+            src="https://utfs.io/f/ZSu459z2V7Otf7XJby463DFYjqOzwx7o842QZhJBUEdHSe9W"
             alt="Windscribe Vpn"
           />
         </div>
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
+        <div className="object-cover border rounded-lg p-5 hover:bg-red-200 transition-all duration-500">
           <img
-            className="w-[125px] "
-            src="https://utfs.io/f/ZSu459z2V7OtGUNsyXL8jSHI1McN5mtXPV4niueRqzkl7Fx6"
+            className=""
+            src="https://utfs.io/f/ZSu459z2V7OtalLiekqmhFxP0TZEq1r3dyKOsUeIaVLjt6bu"
             alt="IPvanish Vpn"
           />
         </div>
 
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
+        <div className="object-cover border rounded-lg p-5 hover:bg-red-200 transition-all duration-500">
           <img
-            className="w-[225px] "
-            src="https://utfs.io/f/ZSu459z2V7OtyI7jbjxm0K2Ht8C1TDIcah3f4jJObPVFU9pQ"
-            alt="PIA Vpn"
+            className=""
+            src="https://utfs.io/f/ZSu459z2V7Ota8ezEHqmhFxP0TZEq1r3dyKOsUeIaVLjt6bu"
+            alt="GPT"
           />
         </div>
 
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
+        <div className="object-cover border rounded-lg p-5 hover:bg-red-200 transition-all duration-500">
           <img
-            className="w-[175px] "
-            src="https://utfs.io/f/ZSu459z2V7OtfAuDNGe463DFYjqOzwx7o842QZhJBUEdHSe9"
-            alt="Windscribe Vpn"
+            className=""
+            src="https://utfs.io/f/ZSu459z2V7OtmWxDI917XiNDb3SAGVW0sLU6lH4EZjMkYPu1"
+            alt="Nord"
           />
         </div>
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
+        <div className="object-cover border rounded-lg p-5 hover:bg-red-200 transition-all duration-500">
           <img
-            className="w-[225px] "
-            src="https://utfs.io/f/ZSu459z2V7Otre0sPyR98gaIEM6N2sbK5i4dUcqhYwrAfOTu"
-            alt="Windscribe Vpn"
-          />
-        </div>
-
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
-          <img
-            className="w-[225px] "
-            src="https://utfs.io/f/ZSu459z2V7OtzQcRyRrulgxUHXQsnWk5p78dfZPySRq1wMr2"
-            alt="Windscribe Vpn"
-          />
-        </div>
-
-        <div className="object-cover border rounded-lg p-5 hover:bg-red-400 transition-all duration-500">
-          <img
-            className="w-[225px] "
-            src="https://utfs.io/f/ZSu459z2V7OtLOh57hsJGqxk6jgY2ESQlbwAWX39nZRiDe0I"
-            alt="Windscribe Vpn"
+            className=""
+            src="https://utfs.io/f/ZSu459z2V7OtlZlSViQWOyu9prv2XUm1R8xjlZKLgE5YHAoT"
+            alt="Mullvad"
           />
         </div>
       </div>
